6de9f5b0385605667a0211dafef1ade0c040c0ec
aravindkarri <aravindkarri98495@gmail.com>
2021-09-07 23:11:23 +0530

    ADDED: repeat till the player reaches the win position

--- src/com/snakeandladdersimulator/SnakeAndLadder.java
+++ src/com/snakeandladdersimulator/SnakeAndLadder.java
@@ -6,40 +6,45 @@ public class SnakeAndLadder
     public static final int NO_PLAY=1;
     public static final int LADDER=2;
     public static final int SNAKE=3;
+    public static final int START_POSITION=0;
+    public static final int WIN=100;
 
     public static void main(String[] args)
     {
-        int START_POSITION = 0;
         int playerPosition=0;
 
         System.out.println("Starting Position is ::"+START_POSITION);
 
-        int ROLL_DICE = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
-        System.out.println("After Rolling Dice We Get ::"+ROLL_DICE);
-
-        int playerCheckOption;
-
-        playerCheckOption = (int)(Math.random()*3 )+1;
-
-
-        switch(playerCheckOption)
+        while(playerPosition <= WIN)
         {
-            case NO_PLAY:
-                System.out.println("It's No Play, Your position is not changed, You are at :" + playerPosition);
-                break;
+            int ROLL_DICE = (int) (Math.floor(Math.random() * 10) % 6) + 1;
+            System.out.println("After Rolling Dice We Get ::" + ROLL_DICE);
+            int playerCheckOption = (int) (Math.random() * 3) + 1;
 
-            case LADDER:
-                playerPosition = playerPosition + ROLL_DICE;
-                System.out.println("Congratulations !! You Got the ladder your position will be increesed by " + ROLL_DICE);
-                break;
+            switch (playerCheckOption)
+            {
+                case NO_PLAY:
+                    System.out.println("It's No Play, Your position is not changed, You are at :" + playerPosition);
+                    break;
 
-            case SNAKE:
-                playerPosition = playerPosition - ROLL_DICE;
-                System.out.println("Oops !! You just got bitten by snake your position will be increesed by " + ROLL_DICE);
-                break;
+                case LADDER:
+                    playerPosition = playerPosition + ROLL_DICE;
+                    System.out.println("Congratulations !! You Got the ladder your position will be increased by " + ROLL_DICE);
+                    break;
 
-            default:
-                System.out.println("Something went wrong!!");
+                case SNAKE:
+                    if ((playerPosition - ROLL_DICE) < START_POSITION) {
+                        playerPosition = START_POSITION;
+                    } else {
+                        playerPosition = playerPosition - ROLL_DICE;
+                        System.out.println("Oops !! You just got bitten by snake your position will be decreased by " + ROLL_DICE);
+                    }
+                    break;
+
+                default:
+                    System.out.println("Something went wrong!!");
+            }
+            System.out.println("Position :: "+playerPosition);
         }
     }
 }
