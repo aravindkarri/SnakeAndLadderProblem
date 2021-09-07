ac39f050b356dab2bd2760fdadbd49db32e6a20e
aravindkarri <aravindkarri98495@gmail.com>
2021-09-07 22:34:40 +0530

    ADDED: rolling the die

--- src/com/snakeandladdersimulator/SnakeAndLadder.java
+++ src/com/snakeandladdersimulator/SnakeAndLadder.java
@@ -5,6 +5,9 @@ public class SnakeAndLadder
     public static void main(String[] args)
     {
         int START_POSITION = 0;
-        System.out.println(START_POSITION);
+        System.out.println("Starting Position is ::"+START_POSITION);
+
+        int ROLL_DICE = (int) (Math.floor(Math.random() * 10 ) % 6) + 1 ;
+        System.out.println("After Rolling Dice We Get ::"+ROLL_DICE);
     }
 }
