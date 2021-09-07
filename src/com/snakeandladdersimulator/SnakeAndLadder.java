e805b4722353f410804e12df5abe5ff04f67bf79
aravindkarri <aravindkarri98495@gmail.com>
2021-09-07 22:29:47 +0530

    ADDED: single player starting position

--- src/com/snakeandladdersimulator/SnakeAndLadder.java
+++ src/com/snakeandladdersimulator/SnakeAndLadder.java
@@ -2,5 +2,9 @@ package com.snakeandladdersimulator;
 
 public class SnakeAndLadder
 {
-    
+    public static void main(String[] args)
+    {
+        int START_POSITION = 0;
+        System.out.println(START_POSITION);
+    }
 }
